8e764dcbc64e6de37315f7a30ded0e38e285d7e5
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-02-21 11:44:38 +0000

    Print 'Buzz' instead of number if number is a multiple of 5, and 'FizzBuzz' if number is a multiple of 3 and 5

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -14,6 +14,10 @@ namespace FizzBuzz
                 {
                     result += "Fizz";
                 }
+                if (i % 5 == 0)
+                {
+                    result += "Buzz";
+                }
                 if (result == "")
                 {
                     result = i.ToString();
